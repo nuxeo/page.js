dd0f2d1569387cf382cf3ea9d4d7eb0088bbb9db
Swarnadipa Choudhury <swarnadipa.choudhury@globallogic.com>
2025-05-28 11:19:56 +0530

    Upgraded rollup version and added plugins to support ES6 syntax

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,22 @@
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
-export default {
-  input: 'index.js',
-  output: {
-    file: 'page.js',
-    format: 'umd',
-    name: 'page'
-  },
-  plugins: [
-    nodeResolve({
-      jsnext: true,
-      main: true
-    }),
-    commonjs({
-      include: ['node_modules/**', '**']
-    })
-  ]
+export default commandLineArgs => {
+  const isMinimized = !!commandLineArgs.minimiz;
+
+  return {
+    input: 'index.js',
+    output: {
+      file: isMinimized ? 'page.min.js' : 'page.js',
+      format: 'umd',
+      name: 'page',
+      sourcemap: true
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      ...(isMinimized ? [terser()] : [])
+    ]
+  };
 };
